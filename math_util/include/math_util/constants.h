753386d5159bf784425ef94baac0f6ada12b91c7
Marc Alban <malban@swri.org>
2013-03-25 14:36:26 -0500

    add math_util package

---
+++ math_util/include/math_util/constants.h
@@ -0,0 +1,42 @@
+// *****************************************************************************
+//
+// Copyright (C) 2013 All Right Reserved, Southwest Research Institute® (SwRI®)
+//
+// Contract No.  10-62987
+// Contractor    Southwest Research Institute® (SwRI®)
+// Address       6220 Culebra Road, San Antonio, Texas 78228-0510
+// Contact       Kris Kozak <kkozak@swri.org> (210) 522-3854
+//
+// This code was developed as part of an internal research project fully funded
+// by Southwest Research Institute®.
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// *****************************************************************************
+
+#ifndef MATH_UTIL_CONSTANTS_H_
+#define MATH_UTIL_CONSTANTS_H_
+
+namespace math_util
+{
+  static const long double _pi = 3.1415926535897932384626433832795029L;
+
+  static const long double _2pi = 6.28318530717958647692528676655900576L;
+
+  static const long double _half_pi = 1.57079632679489661923132169163975144L;
+
+  static const long double _deg_2_rad = 0.01745329251994329576923690768488612L;
+
+  static const long double _rad_2_deg = 57.2957795130823208767981548141051703L;
+
+  static const double _kph_to_mps = 0.2777777777777778;
+
+  static const double _mps_to_mph = 2.2369362920544025;
+
+  static const double _feet_to_meters = 0.3048;
+}
+
+#endif  // MATH_UTIL_CONSTANTS_H_
