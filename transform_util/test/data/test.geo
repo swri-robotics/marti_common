5bb5bcf9c2f9cc1d089e6548c1eba74173742920
Marc Alban <malban@swri.org>
2013-03-29 22:07:25 -0500

    add unit test for loading georeference file

---
+++ transform_util/test/data/test.geo
@@ -0,0 +1,13 @@
+image_path: "."
+image_width: 29184
+image_height: 15872
+tile_size: 512
+
+datum: "wgs84"
+projection: "utm"
+
+tiepoints:
+ - point: [4799, 209, 535674.5, 3258382.5]
+ - point: [2336, 15596, 535329.5, 3256198.5]
+ - point: [26925, 15457, 538837.5, 3256233.5]
+ - point: [29133, 84, 539142.5, 3258416.5]
---
+++ transform_util/test/test_georeference.cpp
@@ -0,0 +1,52 @@
+// *****************************************************************************
+//
+// Copyright (C) 2013 All Right Reserved, Southwest Research Institute® (SwRI®)
+//
+// Contract No.  10-62987
+// Contractor    Southwest Research Institute® (SwRI®)
+// Address       6220 Culebra Road, San Antonio, Texas 78228-0510
+// Contact       Kris Kozak <kkozak@swri.org> (210) 522-3854
+//
+// This code was developed as part of an internal research project fully funded
+// by Southwest Research Institute®.
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// *****************************************************************************
+
+#include <gtest/gtest.h>
+
+#include <ros/ros.h>
+
+#include <transform_util/georeference.h>
+
+TEST(GeoreferenceTests, Load)
+{
+  std::string filename;
+  ASSERT_TRUE(ros::param::get("geo_file", filename));
+
+  transform_util::GeoReference georeference(filename);
+  ASSERT_TRUE(georeference.Load());
+
+  EXPECT_EQ(std::string("wgs84"), georeference.Datum());
+  EXPECT_EQ(std::string("utm"), georeference.Projection());
+  EXPECT_EQ(29184, georeference.Width());
+  EXPECT_EQ(15872, georeference.Height());
+  EXPECT_EQ(512, georeference.TileSize());
+}
+
+// TODO(malban): Test coordinate/pixel transforms.
+
+// Run all the tests that were declared with TEST()
+int main(int argc, char **argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+
+  // Initialize the ROS core parameters can be loaded from the launch file
+  ros::init(argc, argv, "test_georeference");
+
+  return RUN_ALL_TESTS();
+}
