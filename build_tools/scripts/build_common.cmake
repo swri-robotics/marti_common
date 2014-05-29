b4494c86711b8cbac361a7f85f3a982d4ef9494e
Edward Venator <evenator@swri.org>
2014-05-29 10:01:21 -0500

    New build_tools repository.
    Creates new build_tools repository (formerly sumet_build_tools).
    Also updates all packages in this repository to use the new build_tools
    script instead of hardcoded logging.

---
+++ build_tools/scripts/build_common.cmake
@@ -0,0 +1,5 @@
+# Default build script for MARTI ROS packages
+# Anything in this script will be executed for all packages that include it
+
+rosbuild_find_ros_package(build_tools)
+include(${build_tools_PACKAGE_PATH}/scripts/version.cmake)
