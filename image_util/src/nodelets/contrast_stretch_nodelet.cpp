346fb537f02d34556d7c937aba9b2f0aebac5345
Marc Alban <malban@swri.org>
2014-06-04 15:02:05 -0500

    add nodelet to contrast stretch an image

---
+++ image_util/src/nodelets/contrast_stretch_nodelet.cpp
@@ -0,0 +1,80 @@
+// *****************************************************************************
+//
+// Copyright (C) 2014 All Right Reserved, Southwest Research Institute® (SwRI®)
+//
+// Contractor    Southwest Research Institute® (SwRI®)
+// Address       6220 Culebra Road, San Antonio, Texas 78228-0510
+// Contact       Kris Kozak <kkozak@swri.org> (210) 522-3854
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// *****************************************************************************
+
+#include <string>
+
+#include <opencv2/core/core.hpp>
+
+#include <ros/ros.h>
+#include <nodelet/nodelet.h>
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <image_util/image_normalization.h>
+
+#include <math_util/math_util.h>
+
+namespace image_util
+{
+  class ContrastStretchNodelet : public nodelet::Nodelet
+  {
+  public:
+    ContrastStretchNodelet() :
+      bins_(8)
+    {
+    }
+
+    ~ContrastStretchNodelet()
+    {
+    }
+
+    void onInit()
+    {
+      ros::NodeHandle &node = getNodeHandle();
+      ros::NodeHandle &priv = getPrivateNodeHandle();
+
+      priv.param("bins", bins_, bins_);
+
+      image_transport::ImageTransport it(node);
+      image_sub_ = it.subscribe("image", 1, &ContrastStretchNodelet::ImageCallback, this);
+      image_pub_ = it.advertise("normalized_image", 1);
+    }
+
+    void ImageCallback(const sensor_msgs::ImageConstPtr& image)
+    {
+      cv_bridge::CvImagePtr cv_image = cv_bridge::toCvCopy(image);
+
+      image_util::ContrastStretch(bins_, cv_image->image, cv_image->image);
+
+      image_pub_.publish(cv_image->toImageMsg());
+    }
+
+  private:
+    int32_t bins_;
+
+    image_transport::Subscriber image_sub_;
+    image_transport::Publisher image_pub_;
+
+  };
+}
+
+// Register nodelet plugin
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_DECLARE_CLASS(
+    image_util,
+    contrast_stretch,
+    image_util::ContrastStretchNodelet,
+    nodelet::Nodelet)
---
+++ image_util/src/nodes/contrast_stretch.cpp
@@ -0,0 +1,36 @@
+// *****************************************************************************
+//
+// Copyright (C) 2014 All Right Reserved, Southwest Research Institute® (SwRI®)
+//
+// Contract No.  10-R8248
+// Contractor    Southwest Research Institute® (SwRI®)
+// Address       6220 Culebra Road, San Antonio, Texas 78228-0510
+// Contact       Kris Kozak <kkozak@swri.org> (210) 522-3854
+//
+// This code was developed as part of an internal research project fully funded
+// by Southwest Research Institute®.
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// *****************************************************************************
+
+#include <ros/ros.h>
+#include <nodelet/loader.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "contrast_stretch", ros::init_options::AnonymousName);
+
+  nodelet::Loader manager(false);
+
+  nodelet::M_string remappings;
+  nodelet::V_string my_argv;
+  manager.load(ros::this_node::getName(), "image_util/contrast_stretch",
+      remappings, my_argv);
+
+  ros::spin();
+  return 0;
+}
