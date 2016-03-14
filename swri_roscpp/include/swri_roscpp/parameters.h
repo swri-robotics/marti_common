4a9ef3edec22e287cb49317ce6a7f7ec7da7b4ed
Ed Venator <evenator@gmail.com>
2016-03-14 13:17:47 -0500

    Adds the ability to warn when unused parameters are set in a namespace.
    A common error when using unfamiliar ROS nodes is to accidentally set
    parameters by the wrong name. This features allows the node author
    to output a WARNING for any unused parameters.
    
    See the param_test node for an example.

--- swri_roscpp/include/swri_roscpp/parameters.h
+++ swri_roscpp/include/swri_roscpp/parameters.h
@@ -1,15 +1,30 @@
 #ifndef SWRI_ROSCPP_PARAMETERS_H_
 #define SWRI_ROSCPP_PARAMETERS_H_
 
+#include <algorithm>
+#include <set>
+#include <string>
+#include <vector>
+
+#include <boost/bind.hpp>
+
+#include <ros/console.h>
 #include <ros/node_handle.h>
 
 namespace swri
 {
+
+  /// This set stores all of the parameters that have been got with this library
+  /// It is used for the getUnusedParamKeys and WarnUnusedParams functions
+  std::set<std::string> _used_params;
+
   static inline
   bool getParam(const ros::NodeHandle &nh,
       const std::string &name,
       int &variable)
   {
+    std::string resolved_name = nh.resolveName(name);
+    _used_params.insert(resolved_name);
     if (!nh.getParam(name, variable))
     {
       ROS_ERROR("Required int parameter %s does not exist", name.c_str());
@@ -24,6 +39,8 @@ namespace swri
       const std::string &name,
       double &variable)
   {
+    std::string resolved_name = nh.resolveName(name);
+    _used_params.insert(resolved_name);
     if (!nh.getParam(name, variable))
     {
       ROS_ERROR("Required double parameter %s does not exist", name.c_str());
@@ -54,6 +71,8 @@ namespace swri
       const std::string &name,
       std::string &variable)
   {
+    std::string resolved_name = nh.resolveName(name);
+    _used_params.insert(resolved_name);
     if (!nh.getParam(name, variable))
     {
       ROS_ERROR("Required string parameter %s does not exist", name.c_str());
@@ -68,6 +87,8 @@ namespace swri
       const std::string &name,
       bool &variable)
   {
+    std::string resolved_name = nh.resolveName(name);
+    _used_params.insert(resolved_name);
     if (!nh.getParam(name, variable))
     {
       ROS_ERROR("Required bool parameter %s does not exist", name.c_str());
@@ -83,6 +104,8 @@ namespace swri
       int &variable,
       const int default_value)
   {
+    std::string resolved_name = nh.resolveName(name);
+    _used_params.insert(resolved_name);
     nh.param(name, variable, default_value);
     ROS_INFO("Read parameter %s = %d", name.c_str(), variable);
   }
@@ -93,6 +116,8 @@ namespace swri
       double &variable,
       const double default_value)
   {
+    std::string resolved_name = nh.resolveName(name);
+    _used_params.insert(resolved_name);
     nh.param(name, variable, default_value);
     ROS_INFO("Read parameter %s = %lf", name.c_str(), variable);
   }
@@ -116,6 +141,8 @@ namespace swri
       std::string &variable,
       const std::string default_value)
   {
+    std::string resolved_name = nh.resolveName(name);
+    _used_params.insert(resolved_name);
     nh.param(name, variable, default_value);
     ROS_INFO("Read parameter %s = \"%s\"", name.c_str(), variable.c_str());
   }
@@ -126,8 +153,65 @@ namespace swri
       bool &variable,
       const bool default_value)
   {
+    std::string resolved_name = nh.resolveName(name);
+    _used_params.insert(resolved_name);
     nh.param(name, variable, default_value);
     ROS_INFO("Read parameter %s = %s", name.c_str(), variable ? "true" : "false");
+  }
+  
+  inline int comparePrefix(std::string const& string, std::string const& prefix)
+  {
+	return string.compare(0, prefix.size(), prefix);
+  }
+
+  inline bool isPrefixOf(std::string const& prefix, std::string const& string)
+  {
+	return comparePrefix(string, prefix) == 0;
+  }
+
+  inline int prefixLessThan(std::string const& string, std::string const& prefix)
+  {
+	return string.compare(0, prefix.size(), prefix) < 0;
+  }
+  
+  /**
+   * Gets the list of parameters on the parameter server in the namespace of
+   * NodeHandle n that have not yet been got using any of the swri::param() or
+   * swri::getParam() functions.
+   *
+   * The parameter keys are filtered by prefix, so if there exists a parameter
+   * /foo/bar/baz and parameter /foo/bar has been got, then /foo/bar/baz will
+   * not be returned as an unused parameter.
+   *
+   * Note that this function has no way of knowing about parameters got
+   * using the ros::param() and ros::getParam() functions.
+   *
+   * @param n  Nodehandle defining the namespace to restrict the list
+   * @return   A vector of fully-qualified parameter names in n's
+   *           namespace that haven't been got.
+   */
+  std::vector<std::string> getUnusedParamKeys(ros::NodeHandle const& n)
+  {
+	std::string const& n_namespace = n.getNamespace();
+	std::vector<std::string> all_params;
+	n.getParamNames(all_params);
+	std::sort(all_params.begin(), all_params.end());
+	std::vector<std::string> unused_params(all_params.size());
+	boost::function<bool(std::string)> inNamespace = boost::bind(isPrefixOf, n_namespace, _1);
+	std::vector<std::string>::const_iterator first_param_in_namespace = find_if(all_params.begin(), all_params.end(), inNamespace);
+	std::vector<std::string>::const_iterator after_last_param_in_namespace = find_if(all_params.begin(), all_params.end(), std::not1(inNamespace));
+	std::vector<std::string>::iterator last_unused_param = std::set_difference(first_param_in_namespace, after_last_param_in_namespace, _used_params.begin(), _used_params.end(), unused_params.begin(), prefixLessThan);
+	unused_params.resize(last_unused_param - unused_params.begin());
+	return unused_params;
+  }
+
+  void warnUnusedParams(ros::NodeHandle const& n = ros::NodeHandle("~"))
+  {
+	std::vector<std::string> unused_params = getUnusedParamKeys(n);
+	for (std::vector<std::string>::const_iterator itr = unused_params.begin(); itr != unused_params.end(); ++itr)
+	{
+		ROS_WARN("Parameter %s was set, but not used by this node", itr->c_str());
+	}
   }
 }  // namespace swri_param
 #endif  // SWRI_ROSCPP_PARAMETERS_H_
