b39d7a6a8d2f247195a14768cee2eef6b6c70241
Elliot Johnson <elliot.johnson@swri.org>
2016-04-28 23:29:17 -0500

    Add swri_route_util package.
    This commit adds a new package called swri_route_util that provides a
    more user-friendly interface to the marti_nav_msgs Route and RoutPoint
    classes, and a set of useful utilities.  At this point, most of the
    code (except the properties) has been well tested on bag files.

---
+++ swri_route_util/include/swri_route_util/util.h
@@ -0,0 +1,105 @@
+// *****************************************************************************
+//
+// Copyright (c) 2016, Southwest Research Institute® (SwRI®)
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Southwest Research Institute® (SwRI®) nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// *****************************************************************************
+#ifndef SWRI_ROUTE_UTIL_UTIL_H_
+#define SWRI_ROUTE_UTIL_UTIL_H_
+
+#include <marti_nav_msgs/RoutePosition.h>
+#include <swri_transform_util/transform.h>
+
+namespace swri_route_util
+{
+class Route;
+class RoutePoint;
+
+// Transform a route.  The route will be transformed in place using
+// the supplied transform.  Known property types that are affected by
+// the transform are expceted to be transformed properly.  The
+// frame_id of the transformed route will be set to the required
+// target_frame argument, because forgetting to up the frame_id has
+// caused difficult bugs several times.
+void transform(Route &route,
+               const swri_transform_util::Transform &transform,
+               const std::string &target_frame);
+
+
+// Project a route to the XY plane by setting the Z coordinate to zero.
+void projectToXY(Route &route);
+
+
+// Fill in the orientation of the route points using an estimate from
+// the route geometry and desired "up" direction.  This function
+// assumes the route is in a cartesian (e.g. not WGS84) frame.
+void fillOrientations(Route &route,
+                      const tf::Vector3 &up=tf::Vector3(0.0, 0.0, 1.0));
+
+
+// Find the closest point on the route (as a route position) for a
+// given point.  If extrapolate_before_start and/or
+// extrapolate_past_end are true, the projection will consider the
+// first and last segments to extend infinitely (ONLY if the point is
+// nearest to either without extrapolation).  This function assumes
+// the route is in a cartesian (e.g. not WGS84) frame.
+bool projectOntoRoute(marti_nav_msgs::RoutePosition &position,
+                      const Route &route,
+                      const tf::Vector3 &point,
+                      bool extrapolate_before_start,
+                      bool extrapolate_past_end);
+
+
+// Normalize a route position.  A normalize route position is guaranteed to
+// have:
+//   - A valid id for a point in the route.
+//   
+//   - If the position is before the start of the route, the id will be
+//     first route point and the distance will be negative.
+
+//   - If the position is after the end of the route, the id will be the
+//     last route point and the distance will be positive.
+//
+//   - Otherwise, the position's id will be for the point that begins
+//     the segment containing the position, and the distance will be
+//     less than that length of that segment.
+//
+//  This function fails the original position's id is not found in the
+// route.  This function assumes the route is in a cartesian (e.g. not
+// WGS84) frame.
+bool normalizeRoutePosition(marti_nav_msgs::RoutePosition &normalized_position,
+                            const Route &route,
+                            const marti_nav_msgs::RoutePosition &position);
+
+
+// Create a route point from a route position by interpolating between
+// the route's points as needed.  This function assumes the route is
+// in a cartesian (e.g. not WGS84) frame.
+bool interpolateRoutePosition(RoutePoint &point,
+                              const Route &route,
+                              const marti_nav_msgs::RoutePosition &position,
+                              bool allow_extrapolation);
+}  // namespace swri_route_util
+#endif  // SWRI_ROUTE_UTIL_UTIL_H_
