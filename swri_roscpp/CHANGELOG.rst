f4ee606810ed326a3683cda556fb00c8badebf0b
Edward Venator <evenator@swri.org>
2016-05-17 20:21:17 -0500

    Backport swri_roscpp 0.0.9 to hydro.

--- swri_roscpp/CHANGELOG.rst
+++ swri_roscpp/CHANGELOG.rst
@@ -2,9 +2,31 @@
 Changelog for package swri_roscpp
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+0.0.9 (2016-03-04)
+------------------
+* Adds getParam() functions to swri_roscpp.
+  These functions wrap NodeHandle::getParam(). If the parameter does
+  not exist, they emit an error message and return false.
+* Fixes some compiler warnings.
+* The swri::Subscriber::blockTimeouts function now returns a value.
+  Previously, it did not explicitly returning a value, which has
+  undefined behavior. It now returns the result of the blockTimeouts
+  function that it wraps.
+* Contributors: Edward Venator, Elliot Johnson, P. J. Reed
+
+0.0.8 (2016-01-06)
+------------------
+* Makes swri::Subscriber's assignment operator return a value.
+  According to the C++ spec, assignment operators must return a reference to the
+  current object (\*this). swri::Subscriber's assignment operator was not
+  returning a value, which works in GCC, but not Clang. GCC would do the right
+  thing for you, but Clang will compile the code but generate a SIGILL 
+  exception at runtime. This is easily fixed by manually returning \*this.
+* Contributors: P. J. Reed
+
 0.0.7 (2015-11-18)
 ------------------
-* Fixes broken changelog `#279<https://github.com/swri-robotics/marti_common/issues/279>`_.
+* Fixes broken changelog `#279 <https://github.com/swri-robotics/marti_common/issues/279>`_.
 
 0.0.6 (2015-11-17)
 ------------------
--- swri_roscpp/include/swri_roscpp/parameters.h
+++ swri_roscpp/include/swri_roscpp/parameters.h
@@ -5,44 +5,100 @@
 
 namespace swri
 {
-static
-void param(const ros::NodeHandle &nh,
-           const std::string &name,
-           int &variable,
-           const int default_value)
-{
-  nh.param(name, variable, default_value);
-  ROS_INFO("Read parameter %s = %d", name.c_str(), variable);
-}
+  static inline
+  bool getParam(const ros::NodeHandle &nh,
+      const std::string &name,
+      int &variable)
+  {
+    if (!nh.getParam(name, variable))
+    {
+      ROS_ERROR("Required int parameter %s does not exist", name.c_str());
+      return false;
+    }
+    ROS_INFO("Read parameter %s = %d", name.c_str(), variable);
+    return true;
+  }
 
-static
-void param(const ros::NodeHandle &nh,
-           const std::string &name,
-           double &variable,
-           const double default_value)
-{
-  nh.param(name, variable, default_value);
-  ROS_INFO("Read parameter %s = %f", name.c_str(), variable);
-}
+  static inline
+  bool getParam(const ros::NodeHandle &nh,
+      const std::string &name,
+      double &variable)
+  {
+    if (!nh.getParam(name, variable))
+    {
+      ROS_ERROR("Required double parameter %s does not exist", name.c_str());
+      return false;
+    }
+    ROS_INFO("Read parameter %s = %f", name.c_str(), variable);
+    return true;
+  }
 
-static
-void param(const ros::NodeHandle &nh,
-           const std::string &name,
-           std::string &variable,
-           const std::string default_value)
-{
-  nh.param(name, variable, default_value);
-  ROS_INFO("Read parameter %s = \"%s\"", name.c_str(), variable.c_str());
-}
+  static inline
+  bool getParam(const ros::NodeHandle &nh,
+      const std::string &name,
+      std::string &variable)
+  {
+    if (!nh.getParam(name, variable))
+    {
+      ROS_ERROR("Required string parameter %s does not exist", name.c_str());
+      return false;
+    }
+    ROS_INFO("Read parameter %s = %s", name.c_str(), variable.c_str());
+    return true;
+  }
 
-static
-void param(const ros::NodeHandle &nh,
-           const std::string &name,
-           bool &variable,
-           const bool default_value)
-{
-  nh.param(name, variable, default_value);
-  ROS_INFO("Read parameter %s = %s", name.c_str(), variable ? "true" : "false");
-}
+  static inline
+  bool getParam(const ros::NodeHandle &nh,
+      const std::string &name,
+      bool &variable)
+  {
+    if (!nh.getParam(name, variable))
+    {
+      ROS_ERROR("Required bool parameter %s does not exist", name.c_str());
+      return false;
+    }
+    ROS_INFO("Read parameter %s = %s", name.c_str(), variable ? "true" : "false");
+    return true;
+  }
+
+  static inline
+  void param(const ros::NodeHandle &nh,
+      const std::string &name,
+      int &variable,
+      const int default_value)
+  {
+    nh.param(name, variable, default_value);
+    ROS_INFO("Read parameter %s = %d", name.c_str(), variable);
+  }
+
+  static inline
+  void param(const ros::NodeHandle &nh,
+      const std::string &name,
+      double &variable,
+      const double default_value)
+  {
+    nh.param(name, variable, default_value);
+    ROS_INFO("Read parameter %s = %f", name.c_str(), variable);
+  }
+
+  static inline
+  void param(const ros::NodeHandle &nh,
+      const std::string &name,
+      std::string &variable,
+      const std::string default_value)
+  {
+    nh.param(name, variable, default_value);
+    ROS_INFO("Read parameter %s = \"%s\"", name.c_str(), variable.c_str());
+  }
+
+  static inline
+  void param(const ros::NodeHandle &nh,
+      const std::string &name,
+      bool &variable,
+      const bool default_value)
+  {
+    nh.param(name, variable, default_value);
+    ROS_INFO("Read parameter %s = %s", name.c_str(), variable ? "true" : "false");
+  }
 }  // namespace swri_param
 #endif  // SWRI_ROSCPP_PARAMETERS_H_
