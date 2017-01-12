586cae4dbe9bbc20923dab092c9bf1e623085f35
Edward Venator <evenator@swri.org>
2017-01-12 10:57:10 -0600

    Document unused parameter functions.
    The set difference algorithms used to determine which parameters
    are used are non-obvious. This adds documentation.

--- swri_roscpp/include/swri_roscpp/parameters.h
+++ swri_roscpp/include/swri_roscpp/parameters.h
@@ -158,22 +158,52 @@ namespace swri
     nh.param(name, variable, default_value);
     ROS_INFO("Read parameter %s = %s", name.c_str(), variable ? "true" : "false");
   }
-  
+
+  /**
+   * Determines whether `prefix` is a prefix of `string`.
+   *
+   * @param string The longer string to check, which may begin with the
+   *               characters of `prefix`.
+   * @param prefix The shorter string, which may be a prefix of `string`.
+   * @return 0 if the first n characters of `string` are identical to `prefix`,
+   *         where n is the length of `prefix`. 1 or -1 otherwise, according to
+   *         the conventions of std::string::compare()
+   */
   inline int comparePrefix(std::string const& string, std::string const& prefix)
   {
-	return string.compare(0, prefix.size(), prefix);
+    return string.compare(0, prefix.size(), prefix);
   }
 
-  inline bool isPrefixOf(std::string const& prefix, std::string const& string)
+  /**
+   * Boolean wrapper around comparePrefix.
+   *
+   * @param string The longer string to check, which may begin with the
+   *               characters of `prefix`.
+   * @param prefix The shorter string, which may be a prefix of `string`.
+   * @return True if the first n characters of `string` are identical to
+   *         `prefix`, where n is the length of `prefix`, false otherwise.
+   */
+  inline bool isPrefixOf(std::string const& string, std::string const& prefix)
   {
-	return comparePrefix(string, prefix) == 0;
+    return comparePrefix(string, prefix) == 0;
   }
 
+  /**
+   * Less-than wrapper around comparePrefix, for algorithms that require a
+   * less-than comparator, such as sorting.
+   *
+   * @param string The longer string to check, which may begin with the
+   *               characters of `prefix`.
+   * @param prefix The shorter string, which may be a prefix of `string`.
+   * @return True if the first n characters of `string` are "less than" the
+   *         first n to `prefix`, where n is the length of `prefix`, according
+   *         to the conventions of std::string::compare(). False otherwise.
+   */
   inline int prefixLessThan(std::string const& string, std::string const& prefix)
   {
-	return string.compare(0, prefix.size(), prefix) < 0;
+    return comparePrefix(string, prefix) < 0;
   }
-  
+
   /**
    * Gets the list of parameters on the parameter server in the namespace of
    * NodeHandle n that have not yet been got using any of the swri::param() or
@@ -192,26 +222,53 @@ namespace swri
    */
   std::vector<std::string> getUnusedParamKeys(ros::NodeHandle const& n)
   {
-	std::string const& n_namespace = n.getNamespace();
-	std::vector<std::string> all_params;
-	n.getParamNames(all_params);
-	std::sort(all_params.begin(), all_params.end());
-	std::vector<std::string> unused_params(all_params.size());
-	boost::function<bool(std::string)> inNamespace = boost::bind(isPrefixOf, n_namespace, _1);
-	std::vector<std::string>::const_iterator first_param_in_namespace = find_if(all_params.begin(), all_params.end(), inNamespace);
-	std::vector<std::string>::const_iterator after_last_param_in_namespace = find_if(all_params.begin(), all_params.end(), std::not1(inNamespace));
-	std::vector<std::string>::iterator last_unused_param = std::set_difference(first_param_in_namespace, after_last_param_in_namespace, _used_params.begin(), _used_params.end(), unused_params.begin(), prefixLessThan);
-	unused_params.resize(last_unused_param - unused_params.begin());
-	return unused_params;
+    // Get a list of every parameter on the parameter server
+    std::string const& n_namespace = n.getNamespace();
+    std::vector<std::string> all_params;
+    n.getParamNames(all_params);
+
+    // Sort the list of parameters to permit partitioning it by namespace
+    std::sort(all_params.begin(), all_params.end());
+
+    // Partition the list of parameters to only the namespace of interest
+    // Create a functor that tells whether a parameter is in a namespace
+    boost::function<bool(std::string)> inNamespace = boost::bind(isPrefixOf, _1, n_namespace);
+    // Find the beginning of the namespace in the sorted list
+    std::vector<std::string>::const_iterator first_param_in_namespace = find_if(all_params.begin(), all_params.end(), inNamespace);
+    // Find the end of the namespace in the sorted list
+    std::vector<std::string>::const_iterator after_last_param_in_namespace = find_if(all_params.begin(), all_params.end(), std::not1(inNamespace));
+    // At this point, all parameters on the server that are in the namespace
+    // are in range [`first_param_in_namespace`,`after_last_param_in_namespace`)
+
+    // Preallocate a vector to put unused params in.
+    std::vector<std::string> unused_params(all_params.size());
+
+    // Fill unused_params with the set difference (params in namespace) - (used params)
+    std::vector<std::string>::iterator last_unused_param = std::set_difference(
+        first_param_in_namespace, after_last_param_in_namespace,  // Set 1
+        _used_params.begin(), _used_params.end(),                 // Set 2
+        unused_params.begin(),  // Destination iterator
+        prefixLessThan);        // Prefix comparator so that all sub-keys of a
+                                // used parameter are also considered used
+
+    // Shrink the unused_params vector to fit contents
+    unused_params.resize(last_unused_param - unused_params.begin());
+
+    return unused_params;
   }
 
+  /**
+   * Wrapper around getUnusedParamKeys that emits a warning for every unused
+   * parameter
+   * @param n  Nodehandle defining the namespace to restrict the list
+   */
   void warnUnusedParams(ros::NodeHandle const& n = ros::NodeHandle("~"))
   {
-	std::vector<std::string> unused_params = getUnusedParamKeys(n);
-	for (std::vector<std::string>::const_iterator itr = unused_params.begin(); itr != unused_params.end(); ++itr)
-	{
-		ROS_WARN("Parameter %s was set, but not used by this node", itr->c_str());
-	}
+    std::vector<std::string> unused_params = getUnusedParamKeys(n);
+    for (std::vector<std::string>::const_iterator itr = unused_params.begin(); itr != unused_params.end(); ++itr)
+    {
+      ROS_WARN("Parameter %s was set, but not used by this node", itr->c_str());
+    }
   }
 }  // namespace swri_param
 #endif  // SWRI_ROSCPP_PARAMETERS_H_
