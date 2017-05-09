a6f44a09d27c589514c0dba7392d01443e0233a1
P. J. Reed <preed@swri.org>
2017-05-09 17:51:47 +0000

    Handle multiple msg types as origins

--- swri_transform_util/src/nodes/lat_lon_tf_echo.cpp
+++ swri_transform_util/src/nodes/lat_lon_tf_echo.cpp
@@ -31,7 +31,9 @@
 
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
+#include <geographic_msgs/GeoPose.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <gps_common/GPSFix.h>
 
 #include <swri_math_util/constants.h>
 #include <swri_transform_util/local_xy_util.h>
@@ -94,15 +96,51 @@ class LatLonTFEchoNode
     std::string frame_id_;
     std::string fixed_frame_;
 
-    void XYOriginCallback(const geometry_msgs::PoseStampedConstPtr origin)
+    void XYOriginCallback(const topic_tools::ShapeShifter::ConstPtr msg)
     {
-      xy_wgs84_util_.reset(
-          new swri_transform_util::LocalXyWgs84Util(
-              origin->pose.position.y,    // Latitude
-              origin->pose.position.x,    // Longitude
-              0.0,                        // Heading
-              origin->pose.position.z));  // Altitude
-      origin_sub_.shutdown();
+      try
+      {
+        const gps_common::GPSFixConstPtr origin = msg->instantiate<gps_common::GPSFix>();
+        xy_wgs84_util_.reset(
+            new swri_transform_util::LocalXyWgs84Util(
+                origin->latitude,
+                origin->longitude,
+                origin->track,
+                origin->altitude));
+        origin_sub_.shutdown();
+        return;
+      }
+      catch (...) {}
+
+      try
+      {
+        const geometry_msgs::PoseStampedConstPtr origin = msg->instantiate<geometry_msgs::PoseStamped>();
+        xy_wgs84_util_.reset(
+            new swri_transform_util::LocalXyWgs84Util(
+                origin->pose.position.y,    // Latitude
+                origin->pose.position.x,    // Longitude
+                0.0,                        // Heading
+                origin->pose.position.z));  // Altitude
+        origin_sub_.shutdown();
+        return;
+      }
+      catch(...) {}
+
+      try
+      {
+        const geographic_msgs::GeoPoseConstPtr origin = msg->instantiate<geographic_msgs::GeoPose>();
+        xy_wgs84_util_.reset(
+            new swri_transform_util::LocalXyWgs84Util(
+                origin->position.latitude,
+                origin->position.longitude,
+                tf::getYaw(origin->orientation),
+                origin->position.altitude));
+        origin_sub_.shutdown();
+        return;
+      }
+      catch(...) {}
+
+      ROS_ERROR_THROTTLE(1.0, "Unsupported message type received for local_xy_origin.");
     }
 
     void TimerCallback()
