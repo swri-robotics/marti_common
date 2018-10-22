d4d8b12792e6c52e07447b0045ac4e55598dde73
Marc Alban <malban@swri.org>
2018-10-22 17:01:55 -0500

    Add function to test if two line segments intersect. (#525)
    * Add function to test if two line segments intersect.

--- swri_geometry_util/src/intersection.cpp
+++ swri_geometry_util/src/intersection.cpp
@@ -40,10 +40,10 @@
 namespace swri_geometry_util
 {
   bool LineIntersection(
-      cv::Vec2d p1,
-      cv::Vec2d p2,
-      cv::Vec2d p3,
-      cv::Vec2d p4,
+      const cv::Vec2d& p1,
+      const cv::Vec2d& p2,
+      const cv::Vec2d& p3,
+      const cv::Vec2d& p4,
       cv::Vec2d& c)
   {
     double d = (p1[0] - p2[0]) * (p3[1] - p4[1]) - (p1[1] - p2[1]) * (p3[0] - p4[0]);
@@ -59,6 +59,117 @@ namespace swri_geometry_util
     c[1] = n_b / d;
 
     return true;
+  }
+
+  bool LineSegmentIntersection(
+      const cv::Vec2d& p1,
+      const cv::Vec2d& p2,
+      const cv::Vec2d& p3,
+      const cv::Vec2d& p4,
+      cv::Vec2d& c)
+  {
+    // See: "Intersection of two lines in three-space"
+    //      by Ronald Goldman from Graphics Gems
+
+    // Handle case of singe points.
+    if (p1 == p2)
+    {
+      if (PointOnLineSegment(p1, p3, p4))
+      {
+        c = p1;
+        return true;
+      }
+
+      return false;
+    }
+    else if (p3 == p4)
+    {
+      if (PointOnLineSegment(p3, p1, p2))
+      {
+        c = p3;
+        return true;
+      }
+
+      return false;
+    }
+
+    cv::Point2d p(p1);
+    cv::Point2d r(p2 - p1);
+    cv::Point2d q(p3);
+    cv::Point2d s(p4 - p3);
+    cv::Point2d qp(q - p);
+
+    double rs = r.cross(s);
+    if (std::fabs(rs) > std::numeric_limits<float>::epsilon())
+    {
+      // Explicitly divide components since cv::Point doesn't support division
+      // in indigo.
+      double t = qp.cross(cv::Point2d(s.x / rs, s.y / rs));
+      double u = (qp * -1).cross(cv::Point2d(r.x / -rs, r.y / -rs));
+
+      if (u >= 0 && t >= 0 && u <= 1.0 && t <= 1.0)
+      {
+        // The lines intersect within the line segments.
+        c = p + t * r;
+        return true;
+      }
+      else
+      {
+        // The lines intersect, but outside the line segments.
+        return false;
+      }
+    }
+    else if (std::fabs(qp.cross(r)) > std::numeric_limits<float>::epsilon())
+    {
+      // The lines are parellel and non-intersecting.
+      return false;
+    }
+    else
+    {
+      // The lines are parellel and coincident.
+      double rlen = r.dot(r);
+      cv::Point2d unit_r(r.x / rlen, r.y / rlen);
+      double t0 = qp.dot(unit_r);
+      double t1 = t0 + s.dot(unit_r);
+
+      if (t0 > t1)
+      {
+        std::swap(t0, t1);
+      }
+
+      if (t0 <= 1.0 && t1 >= 0.0)
+      {
+        // The line segments overlap.
+
+        double t = std::max(0.0, t0);
+        c = p + t * r;
+        return true;
+      }
+
+      // The line segments don't overlap.
+      return false;
+    }
+  }
+
+  bool PointOnLineSegment(
+      const cv::Vec2d& p1,
+      const cv::Vec2d& p2,
+      const cv::Vec2d& p3)
+  {
+    // Check if the points are collinear.
+    if (((p2[0] - p1[0]) * (p3[1] - p1[0])) - ((p3[0] - p1[0]) * (p2[1] - p1[1])) > std::numeric_limits<float>::epsilon())
+    {
+      return false;
+    }
+
+    if (p2[0] != p3[0])
+    {
+      return (p1[0] <= p3[0] && p2[0] <= p1[0]) || (p1[0] <= p2[0] && p3[0] <= p1[0]);
+    }
+    else
+    {
+      return (p1[1] <= p3[1] && p2[1] <= p1[1]) || (p1[1] <= p2[1] && p3[1] <= p1[1]);
+    }
   }
 
   bool PolygonsIntersect(
--- swri_geometry_util/test/test_intersection.cpp
+++ swri_geometry_util/test/test_intersection.cpp
@@ -93,6 +93,16 @@ TEST(IntersectionTests, Intersects)
 
   EXPECT_FLOAT_EQ(-5.0, c[0]);
   EXPECT_FLOAT_EQ(-5.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineIntersection(
+      cv::Vec2d(0, 5),
+      cv::Vec2d(-10, 5),
+      cv::Vec2d(5, 0),
+      cv::Vec2d(5, -10),
+      c));
+
+  EXPECT_FLOAT_EQ(5.0, c[0]);
+  EXPECT_FLOAT_EQ(5.0, c[1]);
 }
 
 TEST(IntersectionTests, Parallel)
@@ -124,6 +134,394 @@ TEST(IntersectionTests, Parallel)
       cv::Vec2d(10, 0),
       cv::Vec2d(0, -1),
       cv::Vec2d(10, -1),
+      c));
+}
+
+TEST(IntersectionTests, SegmentsIntersect)
+{
+  cv::Vec2d c;
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 5),
+      cv::Vec2d(10, 5),
+      cv::Vec2d(5, 0),
+      cv::Vec2d(5, 10),
+      c));
+
+  EXPECT_FLOAT_EQ(5.0, c[0]);
+  EXPECT_FLOAT_EQ(5.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 6),
+      cv::Vec2d(10, 6),
+      cv::Vec2d(5, 0),
+      cv::Vec2d(5, 10),
+      c));
+
+  EXPECT_FLOAT_EQ(5.0, c[0]);
+  EXPECT_FLOAT_EQ(6.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(10, 6),
+      cv::Vec2d(0, 6),
+      cv::Vec2d(5, 10),
+      cv::Vec2d(5, 0),
+      c));
+
+  EXPECT_FLOAT_EQ(5.0, c[0]);
+  EXPECT_FLOAT_EQ(6.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(5, 10),
+      cv::Vec2d(5, 0),
+      cv::Vec2d(10, 6),
+      cv::Vec2d(0, 6),
+      c));
+
+  EXPECT_FLOAT_EQ(5.0, c[0]);
+  EXPECT_FLOAT_EQ(6.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(10, 10),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(0, 10),
+      cv::Vec2d(10, 0),
+      c));
+
+  EXPECT_FLOAT_EQ(5.0, c[0]);
+  EXPECT_FLOAT_EQ(5.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(-10, -10),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(0, -10),
+      cv::Vec2d(-10, 0),
+      c));
+
+  EXPECT_FLOAT_EQ(-5.0, c[0]);
+  EXPECT_FLOAT_EQ(-5.0, c[1]);
+
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 2),
+      cv::Vec2d(2, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(2, 2),
+      c));
+
+  EXPECT_FLOAT_EQ(1.0, c[0]);
+  EXPECT_FLOAT_EQ(1.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 2),
+      cv::Vec2d(2, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(2, 0),
+      c));
+
+  EXPECT_FLOAT_EQ(2.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(3, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(0, 2),
+      cv::Vec2d(2, 0),
+      c));
+  EXPECT_FLOAT_EQ(2.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 2),
+      cv::Vec2d(2, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(3, 0),
+      c));
+  EXPECT_FLOAT_EQ(2.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 2),
+      cv::Vec2d(2, 0),
+      cv::Vec2d(3, 0),
+      cv::Vec2d(0, 0),
+      c));
+  EXPECT_FLOAT_EQ(2.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 2),
+      cv::Vec2d(2, 0),
+      cv::Vec2d(2, 0),
+      cv::Vec2d(0, 0),
+      c));
+  EXPECT_FLOAT_EQ(2.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 2),
+      cv::Vec2d(2, 0),
+      cv::Vec2d(0, 2),
+      cv::Vec2d(0, 0),
+      c));
+
+  EXPECT_FLOAT_EQ(0.0, c[0]);
+  EXPECT_FLOAT_EQ(2.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 2),
+      cv::Vec2d(2, 0),
+      cv::Vec2d(1, 1),
+      cv::Vec2d(0, 0),
+      c));
+
+  EXPECT_FLOAT_EQ(1.0, c[0]);
+  EXPECT_FLOAT_EQ(1.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(200, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(33.3, 0),
+      cv::Vec2d(10, 10),
+      c));
+  EXPECT_FLOAT_EQ(33.3, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(0, 0),
+      c));
+  EXPECT_FLOAT_EQ(0.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 2),
+      cv::Vec2d(2, 0),
+      cv::Vec2d(1, 1),
+      cv::Vec2d(1, 1),
+      c));
+  EXPECT_FLOAT_EQ(1.0, c[0]);
+  EXPECT_FLOAT_EQ(1.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(1, 1),
+      cv::Vec2d(1, 1),
+      cv::Vec2d(0, 2),
+      cv::Vec2d(2, 0),
+      c));
+  EXPECT_FLOAT_EQ(1.0, c[0]);
+  EXPECT_FLOAT_EQ(1.0, c[1]);
+
+  ASSERT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(1065.8687582537791058712173253,-1053.2999883795632740657310933),
+      cv::Vec2d(1065.5686875431010776082985103,-1051.8000590902413478033849970),
+      cv::Vec2d(1066.1686875431009866588283330,-1053.0000590902413932781200856),
+      cv::Vec2d(1065.5686875431010776082985103,-1051.8000590902413478033849970),
+      c));
+  EXPECT_FLOAT_EQ(1065.5686875431010776082985103, c[0]);
+  EXPECT_FLOAT_EQ(-1051.8000590902413478033849970, c[1]);
+}
+
+TEST(IntersectionTests, SegmentsDontIntersect)
+{
+  cv::Vec2d c;
+  ASSERT_FALSE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 5),
+      cv::Vec2d(-10, 5),
+      cv::Vec2d(5, 0),
+      cv::Vec2d(5, -10),
+      c));
+
+  ASSERT_FALSE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 2),
+      cv::Vec2d(2, 0),
+      cv::Vec2d(2, 1),
+      cv::Vec2d(5, 2),
+      c));
+
+  ASSERT_FALSE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 2),
+      cv::Vec2d(2, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(-5, -5),
+      c));
+
+  ASSERT_FALSE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 2),
+      cv::Vec2d(2, 0),
+      cv::Vec2d(0.999, 0.999),
+      cv::Vec2d(0, 0),
+      c));
+
+  ASSERT_FALSE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(200, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(33.3, 0.0000001),
+      cv::Vec2d(10, 10),
+      c));
+
+  ASSERT_FALSE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(1.00001, 1.00001),
+      cv::Vec2d(1.00001, 1.00001),
+      cv::Vec2d(0, 2),
+      cv::Vec2d(2, 0),
+      c));
+
+  ASSERT_FALSE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 2),
+      cv::Vec2d(2, 0),
+      cv::Vec2d(1.00001, 1.00001),
+      cv::Vec2d(1.00001, 1.00001),
+      c));
+}
+
+TEST(IntersectionTests, ParallelSegments)
+{
+  cv::Vec2d c;
+  EXPECT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 0),
+      cv::Vec2d(10, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(10, 0),
+      c));
+  EXPECT_FLOAT_EQ(0.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  EXPECT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 0),
+      cv::Vec2d(10, 0),
+      cv::Vec2d(10, 0),
+      cv::Vec2d(0, 0),
+      c));
+  EXPECT_FLOAT_EQ(0.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  EXPECT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(10, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(10, 0),
+      cv::Vec2d(0, 0),
+      c));
+  EXPECT_FLOAT_EQ(10.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  EXPECT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(10, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(10, 0),
+      c));
+  EXPECT_FLOAT_EQ(10.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  EXPECT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(10, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(10, 0),
+      cv::Vec2d(20, 0),
+      c));
+  EXPECT_FLOAT_EQ(10.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  EXPECT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(10, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(5, 0),
+      cv::Vec2d(10, 0),
+      c));
+  EXPECT_FLOAT_EQ(10.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  EXPECT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(10, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(10, 0),
+      cv::Vec2d(5, 0),
+      c));
+  EXPECT_FLOAT_EQ(10.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  EXPECT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 0),
+      cv::Vec2d(10, 0),
+      cv::Vec2d(5, 0),
+      cv::Vec2d(10, 0),
+      c));
+  EXPECT_FLOAT_EQ(5.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  EXPECT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 0),
+      cv::Vec2d(10, 0),
+      cv::Vec2d(10, 0),
+      cv::Vec2d(5, 0),
+      c));
+  EXPECT_FLOAT_EQ(5.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  EXPECT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 0),
+      cv::Vec2d(10, 0),
+      cv::Vec2d(5, 0),
+      cv::Vec2d(6, 0),
+      c));
+  EXPECT_FLOAT_EQ(5.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  EXPECT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 0),
+      cv::Vec2d(10, 0),
+      cv::Vec2d(6, 0),
+      cv::Vec2d(5, 0),
+      c));
+  EXPECT_FLOAT_EQ(5.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  EXPECT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(10, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(5, 0),
+      cv::Vec2d(6, 0),
+      c));
+  EXPECT_FLOAT_EQ(6.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  EXPECT_TRUE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(10, 0),
+      cv::Vec2d(0, 0),
+      cv::Vec2d(6, 0),
+      cv::Vec2d(5, 0),
+      c));
+  EXPECT_FLOAT_EQ(6.0, c[0]);
+  EXPECT_FLOAT_EQ(0.0, c[1]);
+
+  EXPECT_FALSE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 0),
+      cv::Vec2d(10, 0),
+      cv::Vec2d(0, 1),
+      cv::Vec2d(10, 1),
+      c));
+
+  EXPECT_FALSE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 0),
+      cv::Vec2d(10, 0),
+      cv::Vec2d(0, .00001),
+      cv::Vec2d(10, .00001),
+      c));
+
+  EXPECT_FALSE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 0),
+      cv::Vec2d(10, 0),
+      cv::Vec2d(11, 0),
+      cv::Vec2d(20, 0),
+      c));
+
+  EXPECT_FALSE(swri_geometry_util::LineSegmentIntersection(
+      cv::Vec2d(0, 0),
+      cv::Vec2d(10, 0),
+      cv::Vec2d(10.00001, 0),
+      cv::Vec2d(20, 0),
       c));
 }
 
