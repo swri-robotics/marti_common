54c041a1b09c928381ebb1e2a85a21e6c10756f2
Marc Alban <malban@swri.org>
2018-10-30 13:12:16 -0500

    Add polygon utility functions. (#526)

--- swri_geometry_util/test/test_geometry_util.cpp
+++ swri_geometry_util/test/test_geometry_util.cpp
@@ -31,7 +31,7 @@
 
 #include <swri_geometry_util/geometry_util.h>
 
-TEST(IntersectionTests, ClosestPointToLinesInvalid)
+TEST(GeometryUtilTests, ClosestPointToLinesInvalid)
 {
   tf::Vector3 point;
   ASSERT_FALSE(swri_geometry_util::ClosestPointToLines(
@@ -78,7 +78,7 @@ TEST(IntersectionTests, ClosestPointToLinesInvalid)
     point));
 }
 
-TEST(IntersectionTests, ClosestPointToLines)
+TEST(GeometryUtilTests, ClosestPointToLines)
 {
   tf::Vector3 point;
   ASSERT_TRUE(swri_geometry_util::ClosestPointToLines(
@@ -140,6 +140,283 @@ TEST(IntersectionTests, ClosestPointToLines)
   EXPECT_FLOAT_EQ(point.x(), 0);
   EXPECT_FLOAT_EQ(point.y(), 0);
   EXPECT_FLOAT_EQ(point.z(), 10);
+}
+
+TEST(GeometryUtilTests, ProjectPointToLineSegment3D)
+{
+  tf::Vector3 p1(0, 0, 0);
+  tf::Vector3 p2(10, 0, 0);
+  tf::Vector3 p3(2, 0, 0);
+  tf::Vector3 p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(p3.x(), p4.x());
+  EXPECT_FLOAT_EQ(p3.y(), p4.y());
+  EXPECT_FLOAT_EQ(p3.z(), p4.z());
+
+  p4 = swri_geometry_util::ProjectToLineSegment(p2, p1, p3);
+  EXPECT_FLOAT_EQ(p3.x(), p4.x());
+  EXPECT_FLOAT_EQ(p3.y(), p4.y());
+  EXPECT_FLOAT_EQ(p3.z(), p4.z());
+
+  p3 = tf::Vector3(0, 0, 0);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(p3.x(), p4.x());
+  EXPECT_FLOAT_EQ(p3.y(), p4.y());
+  EXPECT_FLOAT_EQ(p3.z(), p4.z());
+
+  p3 = tf::Vector3(0, 1, 0);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(0, p4.x());
+  EXPECT_FLOAT_EQ(0, p4.y());
+  EXPECT_FLOAT_EQ(0, p4.z());
+
+  p3 = tf::Vector3(5, -1, 0);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(5, p4.x());
+  EXPECT_FLOAT_EQ(0, p4.y());
+  EXPECT_FLOAT_EQ(0, p4.z());
+
+  p3 = tf::Vector3(5, -1, 10);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(5, p4.x());
+  EXPECT_FLOAT_EQ(0, p4.y());
+  EXPECT_FLOAT_EQ(0, p4.z());
+
+  p3 = tf::Vector3(-5, -1, 10);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(0, p4.x());
+  EXPECT_FLOAT_EQ(0, p4.y());
+  EXPECT_FLOAT_EQ(0, p4.z());
+
+  p3 = tf::Vector3(15, -1, 10);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(10, p4.x());
+  EXPECT_FLOAT_EQ(0, p4.y());
+  EXPECT_FLOAT_EQ(0, p4.z());
+
+  p1 = tf::Vector3(0, 0, 0);
+  p2 = tf::Vector3(10, 10, 10);
+  p3 = tf::Vector3(1, 1, 1);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(1, p4.x());
+  EXPECT_FLOAT_EQ(1, p4.y());
+  EXPECT_FLOAT_EQ(1, p4.z());
+
+  p2 = tf::Vector3(10, 10, 0);
+  p3 = tf::Vector3(0, 10, 0);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(5, p4.x());
+  EXPECT_FLOAT_EQ(5, p4.y());
+  EXPECT_FLOAT_EQ(0, p4.z());
+
+  p4 = swri_geometry_util::ProjectToLineSegment(p2, p1, p3);
+  EXPECT_FLOAT_EQ(5, p4.x());
+  EXPECT_FLOAT_EQ(5, p4.y());
+  EXPECT_FLOAT_EQ(0, p4.z());
+
+  p3 = tf::Vector3(-100, 10, 0);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(0, p4.x());
+  EXPECT_FLOAT_EQ(0, p4.y());
+  EXPECT_FLOAT_EQ(0, p4.z());
+}
+
+TEST(GeometryUtilTests, ProjectPointToLineSegment2D)
+{
+  cv::Vec2d p1(0, 0);
+  cv::Vec2d p2(10, 0);
+  cv::Vec2d p3(2, 0);
+  cv::Vec2d p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(p3[0], p4[0]);
+  EXPECT_FLOAT_EQ(p3[1], p4[1]);
+
+  p4 = swri_geometry_util::ProjectToLineSegment(p2, p1, p3);
+  EXPECT_FLOAT_EQ(p3[0], p4[0]);
+  EXPECT_FLOAT_EQ(p3[1], p4[1]);
+
+  p3 = cv::Vec2d(0, 0);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(p3[0], p4[0]);
+  EXPECT_FLOAT_EQ(p3[1], p4[1]);
+
+  p3 = cv::Vec2d(0, 1);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(0, p4[0]);
+  EXPECT_FLOAT_EQ(0, p4[1]);
+
+  p3 = cv::Vec2d(5, -1);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(5, p4[0]);
+  EXPECT_FLOAT_EQ(0, p4[1]);
+
+  p3 = cv::Vec2d(-5, -1);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(0, p4[0]);
+  EXPECT_FLOAT_EQ(0, p4[1]);
+
+  p3 = cv::Vec2d(15, -1);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(10, p4[0]);
+  EXPECT_FLOAT_EQ(0, p4[1]);
+
+  p1 = cv::Vec2d(0, 0);
+  p2 = cv::Vec2d(10, 10);
+  p3 = cv::Vec2d(1, 1);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(1, p4[0]);
+  EXPECT_FLOAT_EQ(1, p4[1]);
+
+  p2 = cv::Vec2d(10, 10);
+  p3 = cv::Vec2d(0, 10);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(5, p4[0]);
+  EXPECT_FLOAT_EQ(5, p4[1]);
+
+  p4 = swri_geometry_util::ProjectToLineSegment(p2, p1, p3);
+  EXPECT_FLOAT_EQ(5, p4[0]);
+  EXPECT_FLOAT_EQ(5, p4[1]);
+
+  p3 = cv::Vec2d(-100, 10);
+  p4 = swri_geometry_util::ProjectToLineSegment(p1, p2, p3);
+  EXPECT_FLOAT_EQ(0, p4[0]);
+  EXPECT_FLOAT_EQ(0, p4[1]);
+}
+
+TEST(GeometryUtilTests, DistanceFromLineSegment3D)
+{
+  tf::Vector3 p1(0, 0, 0);
+  tf::Vector3 p2(10, 0, 0);
+  tf::Vector3 p3(2, 0, 0);
+  EXPECT_FLOAT_EQ(0, swri_geometry_util::DistanceFromLineSegment(p1, p2, p3));
+  EXPECT_FLOAT_EQ(0, swri_geometry_util::DistanceFromLineSegment(p2, p1, p3));
+
+  p3 = tf::Vector3(0, 0, 0);
+  EXPECT_FLOAT_EQ(0, swri_geometry_util::DistanceFromLineSegment(p2, p1, p3));
+
+  p3 = tf::Vector3(0, 1, 0);
+  EXPECT_FLOAT_EQ(1, swri_geometry_util::DistanceFromLineSegment(p2, p1, p3));
+
+  p3 = tf::Vector3(5, -1, 0);
+  EXPECT_FLOAT_EQ(1, swri_geometry_util::DistanceFromLineSegment(p2, p1, p3));
+
+  p3 = tf::Vector3(5, -1, 10);
+  EXPECT_FLOAT_EQ(std::sqrt(101), swri_geometry_util::DistanceFromLineSegment(p2, p1, p3));
+
+  p3 = tf::Vector3(-5, -1, 10);
+  EXPECT_FLOAT_EQ(std::sqrt(126), swri_geometry_util::DistanceFromLineSegment(p2, p1, p3));
+
+  p3 = tf::Vector3(15, -1, 10);
+  EXPECT_FLOAT_EQ(std::sqrt(126), swri_geometry_util::DistanceFromLineSegment(p2, p1, p3));
+
+  p1 = tf::Vector3(0, 0, 0);
+  p2 = tf::Vector3(10, 10, 10);
+  p3 = tf::Vector3(1, 1, 1);
+  EXPECT_FLOAT_EQ(0, swri_geometry_util::DistanceFromLineSegment(p2, p1, p3));
+
+  p2 = tf::Vector3(10, 10, 0);
+  p3 = tf::Vector3(0, 10, 0);
+  EXPECT_FLOAT_EQ(std::sqrt(50), swri_geometry_util::DistanceFromLineSegment(p2, p1, p3));
+  EXPECT_FLOAT_EQ(std::sqrt(50), swri_geometry_util::DistanceFromLineSegment(p1, p2, p3));
+
+  p3 = tf::Vector3(-100, 10, 0);
+  EXPECT_FLOAT_EQ(std::sqrt(10100), swri_geometry_util::DistanceFromLineSegment(p1, p2, p3));
+}
+
+TEST(GeometryUtilTests, DistanceFromLineSegment2D)
+{
+  cv::Vec2d p1(0, 0);
+  cv::Vec2d p2(10, 0);
+  cv::Vec2d p3(2, 0);
+  EXPECT_FLOAT_EQ(0, swri_geometry_util::DistanceFromLineSegment(p1, p2, p3));
+  EXPECT_FLOAT_EQ(0, swri_geometry_util::DistanceFromLineSegment(p2, p1, p3));
+
+  p3 = cv::Vec2d(0, 0);
+  EXPECT_FLOAT_EQ(0, swri_geometry_util::DistanceFromLineSegment(p2, p1, p3));
+
+  p3 = cv::Vec2d(0, 1);
+  EXPECT_FLOAT_EQ(1, swri_geometry_util::DistanceFromLineSegment(p2, p1, p3));
+
+  p3 = cv::Vec2d(5, -1);
+  EXPECT_FLOAT_EQ(1, swri_geometry_util::DistanceFromLineSegment(p2, p1, p3));
+
+  p1 = cv::Vec2d(0, 0);
+  p2 = cv::Vec2d(10, 10);
+  p3 = cv::Vec2d(1, 1);
+  EXPECT_FLOAT_EQ(0, swri_geometry_util::DistanceFromLineSegment(p2, p1, p3));
+
+  p2 = cv::Vec2d(10, 10);
+  p3 = cv::Vec2d(0, 10);
+  EXPECT_FLOAT_EQ(std::sqrt(50), swri_geometry_util::DistanceFromLineSegment(p2, p1, p3));
+  EXPECT_FLOAT_EQ(std::sqrt(50), swri_geometry_util::DistanceFromLineSegment(p1, p2, p3));
+
+  p3 = cv::Vec2d(-100, 10);
+  EXPECT_FLOAT_EQ(std::sqrt(10100), swri_geometry_util::DistanceFromLineSegment(p1, p2, p3));
+}
+
+
+TEST(GeometryUtilTests, PointInPolygon)
+{
+  std::vector<cv::Vec2d> polygon1;
+  polygon1.push_back(cv::Vec2d(1, 1));
+  polygon1.push_back(cv::Vec2d(1, -1));
+  polygon1.push_back(cv::Vec2d(-1, -1));
+  polygon1.push_back(cv::Vec2d(-1, 1));
+  EXPECT_TRUE(swri_geometry_util::PointInPolygon(polygon1, cv::Vec2d(0, 0)));
+  EXPECT_TRUE(swri_geometry_util::PointInPolygon(polygon1, cv::Vec2d(0.99, 0.99)));
+  EXPECT_TRUE(swri_geometry_util::PointInPolygon(polygon1, cv::Vec2d(-0.99, 0.99)));
+  EXPECT_TRUE(swri_geometry_util::PointInPolygon(polygon1, cv::Vec2d(-0.99, -0.99)));
+  EXPECT_TRUE(swri_geometry_util::PointInPolygon(polygon1, cv::Vec2d(0.99, -0.99)));
+  EXPECT_FALSE(swri_geometry_util::PointInPolygon(polygon1, cv::Vec2d(2, 0)));
+  EXPECT_FALSE(swri_geometry_util::PointInPolygon(polygon1, cv::Vec2d(-2, 0)));
+  EXPECT_FALSE(swri_geometry_util::PointInPolygon(polygon1, cv::Vec2d(2, -2)));
+
+  std::vector<cv::Vec2d> polygon2;
+  polygon2.push_back(cv::Vec2d(1, 1));
+  polygon2.push_back(cv::Vec2d(1, -1));
+  polygon2.push_back(cv::Vec2d(-1, -1));
+  polygon2.push_back(cv::Vec2d(-1, 1));
+  polygon2.push_back(cv::Vec2d(1, 1));
+  polygon2.push_back(cv::Vec2d(1, 1));
+  EXPECT_TRUE(swri_geometry_util::PointInPolygon(polygon2, cv::Vec2d(0, 0)));
+  EXPECT_TRUE(swri_geometry_util::PointInPolygon(polygon2, cv::Vec2d(0.99, 0.99)));
+  EXPECT_TRUE(swri_geometry_util::PointInPolygon(polygon2, cv::Vec2d(-0.99, 0.99)));
+  EXPECT_TRUE(swri_geometry_util::PointInPolygon(polygon2, cv::Vec2d(-0.99, -0.99)));
+  EXPECT_TRUE(swri_geometry_util::PointInPolygon(polygon2, cv::Vec2d(0.99, -0.99)));
+  EXPECT_FALSE(swri_geometry_util::PointInPolygon(polygon2, cv::Vec2d(2, 0)));
+  EXPECT_FALSE(swri_geometry_util::PointInPolygon(polygon2, cv::Vec2d(-2, 0)));
+  EXPECT_FALSE(swri_geometry_util::PointInPolygon(polygon2, cv::Vec2d(2, -2)));
+}
+
+TEST(GeometryUtilTests, DistanceFromPolygon)
+{
+  std::vector<cv::Vec2d> polygon1;
+  polygon1.push_back(cv::Vec2d(1, 1));
+  polygon1.push_back(cv::Vec2d(1, -1));
+  polygon1.push_back(cv::Vec2d(-1, -1));
+  polygon1.push_back(cv::Vec2d(-1, 1));
+  EXPECT_FLOAT_EQ(1.0, swri_geometry_util::DistanceFromPolygon(polygon1, cv::Vec2d(0, 0)));
+  EXPECT_FLOAT_EQ(0.01, swri_geometry_util::DistanceFromPolygon(polygon1, cv::Vec2d(0.99, 0.99)));
+  EXPECT_FLOAT_EQ(0.01, swri_geometry_util::DistanceFromPolygon(polygon1, cv::Vec2d(-0.99, 0.99)));
+  EXPECT_FLOAT_EQ(0.01, swri_geometry_util::DistanceFromPolygon(polygon1, cv::Vec2d(-0.99, -0.99)));
+  EXPECT_FLOAT_EQ(0.01, swri_geometry_util::DistanceFromPolygon(polygon1, cv::Vec2d(0.99, -0.99)));
+  EXPECT_FLOAT_EQ(1, swri_geometry_util::DistanceFromPolygon(polygon1, cv::Vec2d(2, 0)));
+  EXPECT_FLOAT_EQ(1,swri_geometry_util::DistanceFromPolygon(polygon1, cv::Vec2d(-2, 0)));
+  EXPECT_FLOAT_EQ(std::sqrt(2),swri_geometry_util::DistanceFromPolygon(polygon1, cv::Vec2d(2, -2)));
+
+  std::vector<cv::Vec2d> polygon2;
+  polygon2.push_back(cv::Vec2d(1, 1));
+  polygon2.push_back(cv::Vec2d(1, -1));
+  polygon2.push_back(cv::Vec2d(-1, -1));
+  polygon2.push_back(cv::Vec2d(-1, 1));
+  polygon2.push_back(cv::Vec2d(1, 1));
+  polygon2.push_back(cv::Vec2d(1, 1));
+  EXPECT_FLOAT_EQ(1.0, swri_geometry_util::DistanceFromPolygon(polygon2, cv::Vec2d(0, 0)));
+  EXPECT_FLOAT_EQ(0.01, swri_geometry_util::DistanceFromPolygon(polygon2, cv::Vec2d(0.99, 0.99)));
+  EXPECT_FLOAT_EQ(0.01, swri_geometry_util::DistanceFromPolygon(polygon2, cv::Vec2d(-0.99, 0.99)));
+  EXPECT_FLOAT_EQ(0.01, swri_geometry_util::DistanceFromPolygon(polygon2, cv::Vec2d(-0.99, -0.99)));
+  EXPECT_FLOAT_EQ(0.01, swri_geometry_util::DistanceFromPolygon(polygon2, cv::Vec2d(0.99, -0.99)));
+  EXPECT_FLOAT_EQ(1, swri_geometry_util::DistanceFromPolygon(polygon2, cv::Vec2d(2, 0)));
+  EXPECT_FLOAT_EQ(1,swri_geometry_util::DistanceFromPolygon(polygon2, cv::Vec2d(-2, 0)));
+  EXPECT_FLOAT_EQ(std::sqrt(2),swri_geometry_util::DistanceFromPolygon(polygon2, cv::Vec2d(2, -2)));
 }
 
 // Run all the tests that were declared with TEST()
