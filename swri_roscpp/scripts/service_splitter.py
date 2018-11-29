a36e954d80c62afbe37dc1bce9caacf11cec5ee5
Matthew Bries <matthew.bries@swri.org>
2018-11-29 15:42:24 -0600

    Satisfy python

--- swri_roscpp/scripts/service_splitter.py
+++ swri_roscpp/scripts/service_splitter.py
@@ -1,6 +1,6 @@
 # *****************************************************************************
 #
-# Copyright (c) 2018, Southwest Research Institute® (SwRI®)
+# Copyright (c) 2018, Southwest Research Institute (SwRI)
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
