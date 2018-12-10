4a00c0dc731461614f5d4fe40efb4b1704e471ef
Matthew Bries <matthew.bries@swri.org>
2018-12-09 18:28:06 -0600

    Add int and bool parameters

--- swri_roscpp/include/swri_roscpp/dynamic_parameters.h
+++ swri_roscpp/include/swri_roscpp/dynamic_parameters.h
@@ -27,7 +27,8 @@ namespace swri
       Bool = 0,
       Float = 1,
       Double = 2,
-      String = 3
+      Int = 3,
+      String = 4
     };
 
     Type type;
@@ -37,28 +38,28 @@ namespace swri
     union
     {
       double d;
-      float f;
       bool b;
+      int i;
       //std::string s;
     } Default;
     union
     {
       double d;
-      float f;
       bool b;
+      int i;
       //std::string s;
     } Min;
     union
     {
       double d;
-      float f;
       bool b;
+      int i;
     } Max;
     union
     {
       double d;
-      float f;
       bool b;
+      int i;
       //std::string s;
     } Current;
   };
@@ -112,6 +113,46 @@ namespace swri
         }
       }
 
+      for (int i = 0; i < req.config.ints.size(); i++)
+      {
+        dynamic_reconfigure::IntParameter param = req.config.ints[i];
+        std::map<std::string, DynamicValue>::iterator iter = values_.find(param.name);
+        if (iter == values_.end())
+        {
+          ROS_ERROR("Could not find parameter '%s'", param.name.c_str());
+          continue;
+        }
+
+        if (iter->second.type != DynamicValue::Int)
+        {
+          ROS_ERROR("Value '%s' was not a int type.", param.name.c_str());
+          continue;
+        }
+
+        int* v = (int*)iter->second.pointer;
+        *v = param.value;
+      }
+
+      for (int i = 0; i < req.config.ints.size(); i++)
+      {
+        dynamic_reconfigure::IntParameter param = req.config.ints[i];
+        std::map<std::string, DynamicValue>::iterator iter = values_.find(param.name);
+        if (iter == values_.end())
+        {
+          ROS_ERROR("Could not find parameter '%s'", param.name.c_str());
+          continue;
+        }
+
+        if (iter->second.type != DynamicValue::Bool)
+        {
+          ROS_ERROR("Value '%s' was not a bool type.", param.name.c_str());
+          continue;
+        }
+
+        bool* v = (bool*)iter->second.pointer;
+        *v = param.value;
+      }
+
       PublishCurrent(rsp.config);
 
       return true;
@@ -152,6 +193,15 @@ namespace swri
         if (param->second.type == DynamicValue::Bool)
         {
           type = "bool";
+
+          dynamic_reconfigure::BoolParameter desc;
+          desc.name = param->first;
+          desc.value = true;
+          rdesc.max.bools.push_back(desc);
+          desc.value = false;
+          rdesc.min.bools.push_back(desc);
+          desc.value = param->second.Default.b;
+          rdesc.dflt.bools.push_back(desc);
         }
         else if (param->second.type == DynamicValue::Float)
         {
@@ -178,6 +228,19 @@ namespace swri
           rdesc.min.doubles.push_back(desc);
           desc.value = param->second.Default.d;
           rdesc.dflt.doubles.push_back(desc);
+        }
+        else if (param->second.type == DynamicValue::Int)
+        {
+          type = "int";
+
+          dynamic_reconfigure::IntParameter desc;
+          desc.name = param->first;
+          desc.value = param->second.Max.i;
+          rdesc.max.ints.push_back(desc);
+          desc.value = param->second.Min.i;
+          rdesc.min.ints.push_back(desc);
+          desc.value = param->second.Default.i;
+          rdesc.dflt.ints.push_back(desc);
         }
         else if (param->second.type == DynamicValue::String)
         {
@@ -220,6 +283,20 @@ namespace swri
           param.value = *(float*)value->second.pointer;
           config.doubles.push_back(param);
         }
+        else if (value->second.type == DynamicValue::Int)
+        {
+          dynamic_reconfigure::IntParameter param;
+          param.name = value->first;
+          param.value = *(int*)value->second.pointer;
+          config.ints.push_back(param);
+        }
+        else if (value->second.type == DynamicValue::Bool)
+        {
+          dynamic_reconfigure::BoolParameter param;
+          param.name = value->first;
+          param.value = *(bool*)value->second.pointer;
+          config.bools.push_back(param);
+        }
       }
 
       dynamic_reconfigure::GroupState gs;
@@ -250,6 +327,50 @@ namespace swri
       value.Min.d = min;
       value.Max.d = max;
       value.Default.d = default_value;
+      value.pointer = (void*)&variable;
+      values_[name] = value;
+ 
+      std::string resolved_name = nh_.resolveName(name);
+      //_used_params.insert(resolved_name);
+      nh_.param(name, variable, default_value);
+      ROS_INFO("Read dynamic parameter %s = %lf", name.c_str(), variable);
+    }
+
+    inline
+    void get(const std::string &name,
+      int &variable,
+      const int default_value,
+      const std::string description = "None.",
+      const int min = -100,
+      const int max = 100)
+    {
+      DynamicValue value;
+      value.type = DynamicValue::Int;
+      value.description = description;
+      value.Min.i = min;
+      value.Max.i = max;
+      value.Default.i = default_value;
+      value.pointer = (void*)&variable;
+      values_[name] = value;
+ 
+      std::string resolved_name = nh_.resolveName(name);
+      //_used_params.insert(resolved_name);
+      nh_.param(name, variable, default_value);
+      ROS_INFO("Read dynamic parameter %s = %lf", name.c_str(), variable);
+    }
+
+    inline
+    void get(const std::string &name,
+      bool &variable,
+      const bool default_value,
+      const std::string description = "None.")
+    {
+      DynamicValue value;
+      value.type = DynamicValue::Bool;
+      value.description = description;
+      value.Min.b = false;
+      value.Max.b = true;
+      value.Default.b = default_value;
       value.pointer = (void*)&variable;
       values_[name] = value;
  
