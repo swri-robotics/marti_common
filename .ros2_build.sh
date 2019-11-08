3ead6ab0922f24976f10780dfe7b34000749d584
P. J. Reed <preed@swri.org>
2019-11-08 15:08:14 -0600

    Tweak CI

--- .ros2_build.sh
+++ .ros2_build.sh
@@ -4,10 +4,6 @@ apt-get update
 rosdep update
 apt-get install -y build-essential python3-colcon-ros
 
-pushd /ws/src
-git clone --depth 1 -b 2.0.0 https://github.com/ros/diagnostics.git
-popd
-
 cd /ws
 
 rosdep install src --from-paths -i -y
