679bbbce87cc91d9ec83079f93e23e410c61cfe7
Robert Brothers <robert.brothers@swri.org>
2022-09-25 22:21:14 -0400

    Fall back to distutils.core.setup for swri_cli_tools setup.py

--- swri_cli_tools/setup.py
+++ swri_cli_tools/setup.py
@@ -1,4 +1,8 @@
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
