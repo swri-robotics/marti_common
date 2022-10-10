028b0501b34bfa62e99708d3417c47d7310d9c08
P. J. Reed <pjreed@users.noreply.github.com>
2022-10-10 16:49:31 -0500

    Fix ament exports (#693)
    * Fix ament exports
    
    Signed-off-by: P. J. Reed <phillipreed@hatchbed.com>

--- swri_transform_util/src/nodes/lat_lon_tf_echo.cpp
+++ swri_transform_util/src/nodes/lat_lon_tf_echo.cpp
@@ -33,6 +33,38 @@
 
 #ifndef FROM_MSG_WORKAROUND
 #include <tf2/utils.h>
+#else
+#include <geometry_msgs/msg/quaternion.hpp>
+namespace tf2 {
+  inline
+  double getYaw(const geometry_msgs::msg::Quaternion & q)
+  {
+    double yaw;
+
+    double sqw;
+    double sqx;
+    double sqy;
+    double sqz;
+
+    sqx = q.x * q.x;
+    sqy = q.y * q.y;
+    sqz = q.z * q.z;
+    sqw = q.w * q.w;
+
+    // Cases derived from https://orbitalstation.wordpress.com/tag/quaternion/
+    // normalization added from urdfom_headers
+    double sarg = -2 * (q.x * q.z - q.w * q.y) / (sqx + sqy + sqz + sqw);
+
+    if (sarg <= -0.99999) {
+      yaw = -2 * atan2(q.y, q.x);
+    } else if (sarg >= 0.99999) {
+      yaw = 2 * atan2(q.y, q.x);
+    } else {
+      yaw = atan2(2 * (q.x * q.y + q.w * q.z), sqw + sqx - sqy - sqz);
+    }
+    return yaw;
+  }
+}
 #endif
 
 #ifdef USE_TF2_H_FILES
