5bcc8dd99bd8f3b7a2cffc871d8bb9894448fa10
David Anthony <david.anthony@swri.org>
2023-01-31 16:59:22 -0600

    Updating parameter name and adding description

--- swri_roscpp/include/swri_roscpp/node.h
+++ swri_roscpp/include/swri_roscpp/node.h
@@ -1,3 +1,32 @@
+// *****************************************************************************
+//
+// Copyright (c) 2023, Southwest Research Institute® (SwRI®)
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Southwest Research Institute® (SwRI®) nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL SOUTHWEST RESEARCH INSTITUTE BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// *****************************************************************************
+
 #ifndef SWRI_ROSCPP_NODE_H_
 #define SWRI_ROSCPP_NODE_H_
 
@@ -5,6 +34,7 @@
 #include <rclcpp/macros.hpp>
 #include <rclcpp/node.hpp>
 #include <rclcpp/node_options.hpp>
+#include <rcpputils/asserts.hpp>
 
 namespace swri
 {
@@ -36,14 +66,16 @@ public:
 private:
   void add_doc_string(const std::string& doc_string)
   {
+    rcpputils::check_true(doc_string.length() > 0);
+
     auto param_desc = rcl_interfaces::msg::ParameterDescriptor();
 
     param_desc.description = "Documents the node's purpose.";
     param_desc.read_only = true;
 
-    this->declare_parameter("doc_string", doc_string, param_desc);
+    this->declare_parameter("node_documentation", doc_string, param_desc);
   }
-}; // class SwriNode
+}; // class Node
 
 } // namespace swri
 
