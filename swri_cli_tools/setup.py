4eb39a635b297778aa0104163af68c03fb6fb637
David Anthony <djanthony@gmail.com>
2023-09-11 14:36:37 -0500

    Fixing Buildfarm Issues (#716)
    * Cleaning up code to fix build farm errors

--- swri_cli_tools/setup.py
+++ swri_cli_tools/setup.py
@@ -1,5 +1,5 @@
-from setuptools import find_packages
-from setuptools import setup
+import os
+from setuptools import find_packages, setup
 
 package_name = 'swri_cli_tools'
 
@@ -8,11 +8,10 @@ setup(
     version='3.6.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/' + package_name, ['package.xml']),
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name])
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        (os.path.join('share', package_name), ['package.xml']),
     ],
-    install_requires=['ros2cli'],
+    install_requires=['ros2cli', 'setuptools'],
     zip_safe=True,
     author='David Anthony',
     author_email='david.anthony@swri.org',
@@ -21,6 +20,7 @@ setup(
     url='https://github.com/swri-robotics/marti_common',
     keywords=['ROS'],
     classifiers=[
+        'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD 3 Clause',
         'Programming Language :: Python',
