a25101123d38fd70e4c8995058c506931ed7e6df
david.anthony@swri.org <david.anthony@swri.org>
2025-05-21 14:44:02 -0500

    Fixing exporting of targets

--- swri_roscpp/cmake/swri_roscpp-test.cmake
+++ swri_roscpp/cmake/swri_roscpp-test.cmake
@@ -6,18 +6,15 @@ if(BUILD_TESTING)
   target_include_directories(storing_subscriber_test
     PUBLIC
     $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
-    $<INSTALL_INTERFACE:include>
-    ${diagnostic_updater_INCLUDE_DIRS}
-    ${nav_msgs_INCLUDE_DIRS}
-    ${rclcpp_INCLUDE_DIRS}
-  )
+    $<INSTALL_INTERFACE:include>)
+
   target_link_libraries(storing_subscriber_test
-    ${diagnostic_updater_LIBRARIES}
-    ${marti_common_msgs_LIBRARIES}
-    ${nav_msgs_LIBRARIES}
-    ${rclcpp_LIBRARIES}
-    ${std_msgs_LIBRARIES}
-    ${std_srvs_LIBRARIES}
+    ${diagnostic_updater_TARGETS}
+    ${marti_common_msgs_TARGETS}
+    ${nav_msgs_TARGETS}
+    ${rclcpp_TARGETS}
+    ${std_msgs_TARGETS}
+    ${std_srvs_TARGETS}
     )
 
   ## Service server test
@@ -26,17 +23,14 @@ if(BUILD_TESTING)
     PUBLIC
     $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
     $<INSTALL_INTERFACE:include>
-    ${diagnostic_updater_INCLUDE_DIRS}
-    ${rclcpp_INCLUDE_DIRS}
-    ${std_srvs_INCLUDE_DIRS}
   )
   target_link_libraries(service_server_test
-    ${diagnostic_updater_LIBRARIES}
-    ${marti_common_msgs_LIBRARIES}
-    ${nav_msgs_LIBRARIES}
-    ${rclcpp_LIBRARIES}
-    ${std_msgs_LIBRARIES}
-    ${std_srvs_LIBRARIES}
+    ${diagnostic_updater_TARGETS}
+    ${marti_common_msgs_TARGETS}
+    ${nav_msgs_TARGETS}
+    ${rclcpp_TARGETS}
+    ${std_msgs_TARGETS}
+    ${std_srvs_TARGETS}
     )
 
   ## Timer test
@@ -45,16 +39,14 @@ if(BUILD_TESTING)
     PUBLIC
     $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
     $<INSTALL_INTERFACE:include>
-    ${diagnostic_updater_INCLUDE_DIRS}
-    ${rclcpp_INCLUDE_DIRS}
   )
   target_link_libraries(timer_test
-    ${diagnostic_updater_LIBRARIES}
-    ${marti_common_msgs_LIBRARIES}
-    ${nav_msgs_LIBRARIES}
-    ${rclcpp_LIBRARIES}
-    ${std_msgs_LIBRARIES}
-    ${std_srvs_LIBRARIES}
+    ${diagnostic_updater_TARGETS}
+    ${marti_common_msgs_TARGETS}
+    ${nav_msgs_TARGETS}
+    ${rclcpp_TARGETS}
+    ${std_msgs_TARGETS}
+    ${std_srvs_TARGETS}
     )
 
   rosidl_generate_interfaces(${PROJECT_NAME}_test
@@ -68,18 +60,14 @@ if(BUILD_TESTING)
   target_link_libraries(topic_service_test_server
     Boost::system
     "${cpp_typesupport_target}"
-  )
-  target_link_libraries(topic_service_test_server
-    ${rclcpp_LIBRARIES}
-    ${marti_common_msgs_LIBRARIES}
-  )
+    ${marti_common_msgs_TARGETS}
+    rclcpp::rclcpp)
+
   target_include_directories(topic_service_test_server
     PUBLIC
     $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
-    $<INSTALL_INTERFACE:include>
-    ${rclcpp_INCLUDE_DIRS}
-    ${marti_common_msgs_INCLUDE_DIRS}
-  )
+    $<INSTALL_INTERFACE:include>)
 
+  ament_export_dependencies(Boost)
   ament_export_dependencies(rosidl_default_runtime)
 endif()
