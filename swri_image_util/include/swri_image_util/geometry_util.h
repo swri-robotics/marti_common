46e23d2281fa85216e2110b8a5a8efe4dbbdaa8d
David Anthony <djanthony@gmail.com>
2026-09-11 16:48:46 -0500

    Adding function to get ellipse points with z-value populated

--- swri_image_util/include/swri_image_util/geometry_util.h
+++ swri_image_util/include/swri_image_util/geometry_util.h
@@ -67,6 +67,10 @@ namespace swri_image_util
   /**
    * Gets a list of points on the perimeter of an ellipse.
    *
+   * The Z coordinate of every returned point is 0, whatever the center's Z is.
+   * Use GetEllipsePointsWithZ() to get the perimeter in the plane of the
+   * center instead.
+   *
    * @param[in]  ellipse     The ellipse represented as a 2x2 float matrix.
    * @param[in]  center      The center of the ellipse.
    * @param[in]  scale       A scale factor.
@@ -76,6 +80,27 @@ namespace swri_image_util
    *          An empty list otherwise.
    */
   std::vector<tf2::Vector3> GetEllipsePoints(
+      const cv::Mat& ellipse,
+      const tf2::Vector3& center,
+      double scale,
+      int32_t num_points);
+
+  /**
+   * Gets a list of points on the perimeter of an ellipse, in the plane of the
+   * ellipse's center.
+   *
+   * This is GetEllipsePoints() except that the Z coordinate of each returned
+   * point is the center's Z rather than 0.
+   *
+   * @param[in]  ellipse     The ellipse represented as a 2x2 float matrix.
+   * @param[in]  center      The center of the ellipse.
+   * @param[in]  scale       A scale factor.
+   * @param[in]  num_points  The number of points to use.
+   *
+   * @returns A list of points on the perimeter of the ellipse if successful.
+   *          An empty list otherwise.
+   */
+  std::vector<tf2::Vector3> GetEllipsePointsWithZ(
       const cv::Mat& ellipse,
       const tf2::Vector3& center,
       double scale,
